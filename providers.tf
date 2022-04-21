437e6f05e490d2ee7434f5745ff8802766e0f221
Giridhar Kolli <Giridhar.Kolli@lvs1.com>
2022-04-21 12:21:29 -0700

    updated changes

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.91.0"
+      version = ">= 2.88.1, < 3.0.0"
     }
   }
 }
@@ -15,3 +15,8 @@ provider "azurerm" {
     }
   }
 }
+
+# provider "azurerm" {
+#   features {}
+#   subscription_id = ""
+# }
